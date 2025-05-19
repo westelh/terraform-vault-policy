8632b99c3d5b5a7135d40dbbe1811b2d7f316f0d
Taisei Nishiumi <westelh@gmail.com>
2025-05-19 22:37:44 +0900

    Add outputs

---
+++ output.tf
@@ -0,0 +1,9 @@
+output "name" {
+   value = vault_policy.main.name
+   description = "Generated policy name"
+}
+
+output "id" {
+   value = vault_policy.main.id
+   description = "Generated policy id"
+}
